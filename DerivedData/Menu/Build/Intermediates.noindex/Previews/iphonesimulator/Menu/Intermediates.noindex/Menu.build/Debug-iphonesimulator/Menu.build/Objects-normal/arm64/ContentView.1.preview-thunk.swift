5b6373dc4914be34d500d44633e328e30e1dc48e
Jason Wan <njasow@g.ucla.edu>
2024-04-21 07:20:58 -0400

    Initial Commit

---
+++ DerivedData/Menu/Build/Intermediates.noindex/Previews/iphonesimulator/Menu/Intermediates.noindex/Menu.build/Debug-iphonesimulator/Menu.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
@@ -0,0 +1,32 @@
+@_private(sourceFile: "ContentView.swift") import Menu
+import func SwiftUI.__designTimeBoolean
+import func SwiftUI.__designTimeInteger
+import protocol SwiftUI.PreviewProvider
+import func SwiftUI.__designTimeString
+import func SwiftUI.__designTimeFloat
+import struct SwiftUI.EmptyView
+import protocol SwiftUI.View
+import SwiftUI
+
+extension ContentView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/jasonwan/Code/Tutorial Projects/Swift Stuff/Menu/Menu/ContentView.swift", line: 12)
+        VStack {
+            Image(systemName: __designTimeString("#68634.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: "globe"))
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(__designTimeString("#68634.[1].[0].property.[0].[0].arg[0].value.[1].arg[0].value", fallback: "Hello, world!"))
+        }
+        .padding()
+    
+#sourceLocation()
+    }
+}
+
+import struct Menu.ContentView
+#Preview {
+    ContentView()
+}
+
+
+
---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by jason wan on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
